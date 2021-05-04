7bf6e383cf0a232a7de9a034d25917676737a1f4
cjamit7 <cjamit@hotmail.com>
2021-05-03 23:01:03 -0500

    Add files via upload

---
+++ Practica 2 firebase/JS/basededatos.js
@@ -0,0 +1,12 @@
+  
+var firebaseConfig = {
+    apiKey: "AIzaSyCrXFL_mMbyLRVSC9Fn6_lfsDjMx54EzlE",
+    authDomain: "practica1-e1b15.firebaseapp.com",
+    projectId: "practica1-e1b15",
+    storageBucket: "practica1-e1b15.appspot.com",
+    messagingSenderId: "61070507903",
+    appId: "1:61070507903:web:465cc3c7e6a6393745244a",
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
